807e6be15f49fd173803aaeb090ec7edff6e070a
zakariahka <102551487+zakariahka@users.noreply.github.com>
2024-07-15 22:37:58 -0500

    removed comments

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,23 +8,18 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-    # Load configurations
+
     app.config.from_object(Config)
     app.config.from_pyfile('config.py', silent=True)
     
-    # Initialize MongoDB
     client = MongoClient(app.config['MONGO_URI'])
     db = client.twiscord
     app.db = db
     app.db.users.create_index([("email", pymongo.ASCENDING)], unique=True)
     
-    # Initialize JWT
     jwt = JWTManager(app)
     app.config['JWT_SECRET_KEY'] = app.config.get('SECRET_KEY')
     
-    # Initialize CORS
-    
-    # Register blueprints
     from .blueprints.user import user_bp
     app.register_blueprint(user_bp, url_prefix='/user')
     
